c232fdde155e657180bd1708a0e82f1f6808549f
user112489 <Ali@mohammads-macbook-pro.local>
2016-10-16 22:51:13 +0300

    Initial Commit

---
+++ Instagram Clone Firebase/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Instagram Clone Firebase
+//
+//  Created by Ali Akkawi on 10/16/16.
+//  Copyright © 2016 Ali Akkawi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
